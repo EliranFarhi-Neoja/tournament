0ca9f578736719aff447c5aede4b7ee5673c8277
EliranFarhi-Neoja <160390467+EliranFarhi-Neoja@users.noreply.github.com>
2024-10-23 14:45:17 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ const SECRET_KEY = process.env.SECRET_KEY;
 app.use(json());
 
 app.use(cors({
-    origin: ["https://tournament-gray.vercel.app", "http://localhost:3000"],
+    origin: ["https://neoja-tournament.vercel.app", "http://localhost:3000"],
     methods: ['POST', 'GET', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     credentials: true
 }));
